5db88a74d9e0f8222b5c4c57c125cd9dea81cddd
Morshinka <tegarprarama461@gmail.com>
2024-03-24 03:28:06 +0700

    first commit

---
+++ mengambil_input_data_user.py
@@ -0,0 +1,22 @@
+#input data string
+data = input("masukan data:")
+print (data, type(data))
+#end input data string
+
+#input data integer
+data = int(input("masukan angka :"))
+print (data, type(data))
+
+#input data float
+data1 = float(input("masukan angka :"))
+print (data1, type(data1))
+#end data integer
+
+#input data boolean
+
+#mendeklarasikan inputan menjadi boolen lalu unutk mendeteksi bahwa niali tersebut boolean adalah ubah inputan menjadi integer
+data_bol = bool(int(input("masukan sesuatu : ")))
+print ("data = ", data_bol, type(data_bol))
+
+#end input data boolean
+
---
+++ operasi_aritmatika.py
@@ -0,0 +1,39 @@
+#opreasi aritmatika
+
+a = 5
+b = 10
+
+#penjumlahan
+hasil = a + b
+print (a, "+", b, "=", hasil)
+
+#pengurangan
+kurang = a - b
+print (a, "-", b, "=", kurang)
+
+#perkalian
+kali = a * b
+print (a, "*", b, "=", kali)
+
+#pembagian
+bagi = b / a
+print (a, "/", b, "=", bagi)
+
+#operasi exponen (pangkat)
+pangkat = b ** a
+print (a, "**", b, "=", pangkat)
+
+#opreasi modulus (sisa pembagian)
+var_a = 11
+var_b = 2
+mod = var_a % var_b
+print (a, "%", b, "=", mod)
+
+#opreasi floor division (akan dibulatkan ke bawah) 
+var_a = 13
+var_b = 2
+mod = var_a // var_b
+print (a, "//", b, "=", mod)
+
+#prioritas
+#data dalam kurung > pangkat > perkalian > penjumlahan > pengurangan
